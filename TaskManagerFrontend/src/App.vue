fe39b734065918c37f0ace1192692ecfbfc29627
Petra Puljić <ppuljicc@gmail.com>
2025-01-04 17:15:06 +0100

    Task.vue & App.vue promjene

--- TaskManagerFrontend/src/App.vue
+++ TaskManagerFrontend/src/App.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import Task from './components/Task.vue';
+</script>
 
 <template>
   <div class="max-w-4xl mx-auto p-4">
@@ -17,27 +19,7 @@
     <div class="bg-white p-4 shadow rounded-md">
       <h2 class="text-xl font-semibold text-gray-800 mb-4">Vaši zadaci</h2>
       <ul class="space-y-4">
-        <!-- Task Item -->
-        <li
-          class="flex justify-between items-center p-4 bg-gray-50 rounded-md shadow">
-          <div>
-            <p class="text-lg font-medium text-gray-800">Naslov zadatka</p>
-            <p class="text-sm text-gray-600">Opis zadatka</p>
-          </div>
-          <!-- Task Actions -->
-          <div class="flex space-x-2">
-            <button
-              class="bg-green-500 text-white px-3 py-1 rounded-md hover:bg-green-600">
-              Dovršeno
-            </button>
-            <button
-              class="bg-red-500 text-white px-3 py-1 rounded-md hover:bg-red-600">
-              Obriši
-            </button>
-          </div>
-          <!--/Task Actions-->
-        </li>
-        <!--/Task Item-->
+        <Task naslov="Naslov zadatka" opis="Opis zadatka" />
       </ul>
     </div>
   </div>
