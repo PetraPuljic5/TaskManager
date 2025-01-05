2b7f402e6b0976fa367978e0b09a4cebe2d9e5a3
Petra Puljić <ppuljicc@gmail.com>
2025-01-05 20:50:42 +0100

    MongoDB GET & POST

--- TaskManagerBackend/routes/tasks.js
+++ TaskManagerBackend/routes/tasks.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { connectToDatabase } from "../db.js";
 
 const router = express.Router();
 
@@ -8,8 +9,42 @@ let tasks = [
     { id: 3, naslov: "Riješi zadaću iz UPP-a", opis: "Natjeraj se riješiti zadaću!", zavrsen: false },
 ];
 
-router.get("/", (req, res) => {
-    res.json(tasks);
+router.get("/", async (req, res) => {
+    try {
+      const db = await connectToDatabase();
+      const collection = db.collection("tasks");
+      const tasks = await collection.find({}).toArray();
+      res.json(tasks);
+    } catch (error) {
+      console.error("Greska u dohvacanju", error);
+      res.status(500).send("Greska u dphvacanju.");
+    }
+});
+
+router.post("/", async (req, res) => {
+    try {
+      const db = await connectToDatabase();
+      const collection = db.collection("tasks");
+  
+      const tasks = req.body;
+  
+      if (!Array.isArray(tasks)) {
+        return res.status(400).json({ error: "Nije polje." });
+      }
+      for (const task of tasks) {
+        if (!task.naslov || !task.opis || typeof task.zavrsen !== "boolean") {
+          return res
+            .status(400)
+            .json({ error: "Nedostaju podaci" });
+        }
+      }
+  
+      await collection.insertMany(tasks);
+      res.status(201).json({ message: "Zadaci uspjesno dodani" });
+    } catch (error) {
+      console.error("Greska u dodavanju", error);
+      res.status(500).send("Greska u dodavanju.");
+    }
   });
   
 export default router;
